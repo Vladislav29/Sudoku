f9af1a57e16adc82767a3d7df32ab84ff155d1c1
Veinard <vlad73529@rambler.ru>
2017-01-13 12:52:54 +0300

    init

--- tests.py
+++ tests.py
@@ -1,5 +1,14 @@
-import unittests
+import unittest
 import subprocess
 
-class SudokuTestCase(unittests.TestCase):
+class SudokuTestCase(unittest.TestCase):
     def test_main(self):
+        subprocess.call(['python', 'main.py', 'puzzleOne.txt', 'output.txt'])
+        with open('output.txt', 'r') as f:
+            output = f.read()
+        with open('expected_output.txt', 'r') as f:
+            expected_output = f.read()
+        self.assertEqual(output, expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
